0d97e14a8b7a0ed2b076680c53e5052a5fa7c427
xiayichen <xiayichen@bevol.cn>
2020-03-23 19:09:15 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -5,5 +5,10 @@ import Toast from './components/Toast';
 Vue.config.productionTip = false;
 Vue.use(Toast)
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  data() {
+    return {
+      test: 999999999
+    }
+  },
 }).$mount("#app");
